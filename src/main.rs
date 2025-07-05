d4f9069829d63152d324d34a06eafc1f7d154e2c
lvcky <mail@johnodonnell.xyz>
2025-07-05 16:48:28 -0400

    chore: init instance

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
+use ash::{Entry,Instance, vk};
 use std::{error::Error, result::Result};
-
-struct VulkanApp;
+struct VulkanApp {
+    _entry: Entry,
+    instance: Instance,
+}
 
 impl VulkanApp {
-    fn new() -> Result<Self, Box<dyn Error>> {
-        log::info!("Creating Application");
-        Ok(Self)
+    fn new() -> Result<Self,Box<dyn Error>> {
+        log::debug!("Creating Application");
+        let entry = unsafe {Entry::load()?};
+        let app_info = vk::ApplicationInfo {
+            api_version: vk::make_api_version(0,1,0,0),
+            ..Default::default()
+        };
+        let create_info = vk::InstanceCreateInfo {
+            p_application_info: &app_info,
+            ..Default::default()
+        };
+        let instance: Instance = unsafe { entry.create_instance(&create_info, None)?};
+
+        Ok(Self {
+            _entry : entry,
+            instance
+        })
     }
+
     fn run(&mut self) {
-        log::info!("Running Application");
+        log::debug!("Running Application");
+    }
+
+    fn init_vulkan() {
+         log::debug!("Init Vulkan");
+    }
+    fn main_loop() {
+        log::debug!("Main Loop");       
+    }
+    fn cleanup() {
+        log::debug!("cleanup");
     }
 }
 
