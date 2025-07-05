064078978c302cf48db184377e4c514c84ace19b
lvcky <mail@johnodonnell.xyz>
2025-07-05 17:49:14 -0400

    chore: init instance

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,42 @@
-use ash::{Entry,Instance, vk};
+use ash::{Entry, Instance, vk};
 use std::{error::Error, result::Result};
+use winit::{dpi::LogicalSize, event_loop::EventLoop, window::Window, window::WindowBuilder};
 struct VulkanApp {
+    _event_loop: EventLoop<()>,
+    // _window: Window,
     _entry: Entry,
     instance: Instance,
 }
 
 impl VulkanApp {
-    fn new() -> Result<Self,Box<dyn Error>> {
+    fn new() -> Result<Self, Box<dyn Error>> {
         log::debug!("Creating Application");
-        let entry = unsafe {Entry::load()?};
-        let app_info = vk::ApplicationInfo {
-            api_version: vk::make_api_version(0,1,0,0),
+
+        let event_loop = EventLoop::new()?;
+        let entry: Entry = unsafe { Entry::load()? };
+        // let window = WindowBuilder::new()
+        //     .with_title("Vulkan tutorial with Ash")
+        //     .with_
+        let app_info: vk::ApplicationInfo<'_> = vk::ApplicationInfo {
+            api_version: vk::make_api_version(0, 1, 0, 0),
             ..Default::default()
         };
-        let create_info = vk::InstanceCreateInfo {
+        let create_info: vk::InstanceCreateInfo<'_> = vk::InstanceCreateInfo {
             p_application_info: &app_info,
             ..Default::default()
         };
-        let instance: Instance = unsafe { entry.create_instance(&create_info, None)?};
+        let instance: Instance = unsafe { entry.create_instance(&create_info, None)? };
 
         Ok(Self {
-            _entry : entry,
-            instance
+            _event_loop: event_loop,
+            // _window: window,
+            _entry: entry,
+            instance,
         })
     }
 
     fn run(&mut self) {
         log::debug!("Running Application");
-    }
-
-    fn init_vulkan() {
-         log::debug!("Init Vulkan");
-    }
-    fn main_loop() {
-        log::debug!("Main Loop");       
-    }
-    fn cleanup() {
-        log::debug!("cleanup");
     }
 }
 
